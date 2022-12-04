7d1ced64b62a210348d185e3952e50a97b09dbb4
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2022-12-04 17:36:03 +0000

    smile

--- serve.cjs
+++ serve.cjs
@@ -9,7 +9,9 @@ const dir = path.join(__dirname, './dist')
 const app = express()
 
 app.use(express.static(dir))
-app.use((req, res) => res.sendFile(path.join(dir, './index.html')))
+app.use((req, res) => {
+  res.sendFile(path.join(dir, './index.html'))
+})
 
 app.listen(3000, () => {
   console.log('App listening on port 3000...')
