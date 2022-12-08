9dcaee9341dc1522123a77191ca7e97e811a81ee
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2022-12-08 14:29:48 +0000

    npm-is-easy

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
-import extraSlice from '$slice/extra'
 import userSlice from '$slice/user'
 
-export const baseURL = 'https://baby-todo.onrender.com'
-// export const baseURL = 'http://localhost:8000'
+// export const baseURL = 'https://baby-todo.onrender.com'
+export const baseURL = 'http://localhost:8000'
 
 export const instance = axios.create({
   baseURL,
--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,7 @@ import api from '$api'
 
 window.cn = className
 
+import '../css-reset'
 import '$styles/index.scss'
 import App from './App'
 import AppError from '$components/Error'
